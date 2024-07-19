7cefa143e045ea81a28349f38294bca81a3dfa00
WPP <94626019+Weipeng-95@users.noreply.github.com>
2024-07-19 15:13:21 +0200

    Greeting nach Tagszeit

--- src/lib/dashboard/greeting.svelte
+++ src/lib/dashboard/greeting.svelte
@@ -1,9 +1,23 @@
 <script>
-    let userName ="Username";
+    let userName ="Jason";
+    let greeting = "Tag";
+
+const updateGreeting = () => {
+    const currentHour = new Date().getHours();
+    if (currentHour < 12) {
+        greeting = "Morgen";
+    } else if (currentHour < 18) {
+        greeting = "Tag";
+    } else {
+        greeting = "Abend";
+    }
+};
+
+updateGreeting();
 </script>
 
 <h1
-    class="mt-40 mb-8 text-5xl font-black text-black leading-[62px] max-md:mt-28 max-md:max-w-full max-md:text-4xl max-md:leading-[35px]"
+    class="mb-8 text-5xl font-black text-black leading-[62px] max-md:mt-28 max-md:max-w-full max-md:text-4xl max-md:leading-[35px]"
 >
-    Guten Tag,<br />{userName}
+    Guten {greeting},<br />{userName}
 </h1>
